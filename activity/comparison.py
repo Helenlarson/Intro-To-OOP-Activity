ca018e68f34ebad2ba5bd8ca0785ceaac5e068b6
Helen <lelinewborncare@gmail.com>
2025-09-28 19:34:51 -0700

    I did some changes on the functions adding return

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,12 @@
 # add your get_student_with_more_classes function here!
+def get_student_with_more_classes(student1, student2):
+    """
+    Compares two students and returns the one with more classes.
+    If they have the same number, it returns None.
+    """
+    if student1.get_num_classes() > student2.get_num_classes():
+        return student1
+    elif student2.get_num_classes() > student1.get_num_classes():
+        return student2
+    else:
+        return None
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,16 @@
 # add your Student class here!
+class Student:
+    def __init__(self, name, year, classes):
+        self.name = name 
+        self.year = year 
+        self.classes = classes
+
+    def add_classes(self, subject):
+        self.classes.append(subject)
+        return self.classes
+    
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        return f"{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes"
